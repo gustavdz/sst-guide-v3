db56259cadab44b7df15075516b374ff74e7e73b
gustavdz <gustavdz@gmail.com>
2024-08-24 09:27:56 -0500

    feat(infra): adding the API

--- infra/api.ts
+++ infra/api.ts
@@ -1,8 +1,16 @@
-import { bucket } from "./storage";
-
-export const api = new sst.aws.ApiGatewayV2("Api");
-
-api.route("GET /", {
-  link: [bucket],
-  handler: "packages/functions/src/api.handler",
+import { table } from "./storage";
+export const api = new sst.aws.ApiGatewayV2("Api", {
+  transform: {
+    route: {
+      handler: {
+        link: [table],
+      },
+    },
+  },
 });
+
+api.route("POST /notes", "packages/functions/src/notes/create.main");
+api.route("GET /notes/{id}", "packages/functions/src/notes/get.main");
+api.route("GET /notes", "packages/functions/src/notes/list.main");
+api.route("PUT /notes/{id}", "packages/functions/src/update.main");
+api.route("DELETE /notes/{id}", "packages/functions/src/delete.main");
